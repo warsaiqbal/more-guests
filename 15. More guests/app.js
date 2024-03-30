7baafb496d6951cc908cfd53de5bcea7aadbaac0
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-03-30 09:15:14 -0700

    Add files via upload

---
+++ 15. More guests/app.js
@@ -0,0 +1,13 @@
+var guests = ["Alishba", "Faizan", "Adan"];
+console.log("I'd like to invite", guests[0], "to dinner.");
+console.log("I'd like to invite", guests[1], "to dinner.");
+console.log("I'd like to invite", guests[2], "to dinner.");
+console.log("Unfortunately", guests[2], "can't make it so I'm inviting Hammad to dinner.");
+var guests2 = ["Maryum", "Alishba", "Hafsa", "Faizan", "Hammad", ("Meher")];
+console.log("Now I'm inviting", guests2[1], "to dinner.");
+console.log("I'm inviting", guests2[3], "to dinner.");
+console.log("I'm inviting", guests2[4], "to dinner.");
+console.log("I've found a bigger dinner table so I'd like to invite three more people to join us.");
+console.log("I'm also inviting", guests2[0], "to dinner.");
+console.log("I'm also inviting", guests2[2], "to dinner.");
+console.log("I'm also inviting", guests2[5], "to dinner");
